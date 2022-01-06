d8e5f92ae742e28b9b0a4bc475a58a88b4af0cec
Cory Fabian <Coryf65@gmail.com>
2022-01-05 21:20:04 -0600

    initial worker service

--- WebsiteStatus/README.md
+++ WebsiteStatus/README.md
@@ -1,1 +1,17 @@
-﻿
+﻿# Website Status Checking Service
+
+- A worker service runs all the time with no input.
+
+- on Linux these are Deamons
+- could also run on Azure
+
+- a Worker is able to test and see it in a console, when installed the console is hidden and runs as a service
+
+## How to see this running
+
+- Can see the service in windows in `Task Manager` and the `Services` tab.
+
+## How to Install Worker Service
+
+## How to Uninstall Service
+
--- WebsiteStatus/Worker.cs
+++ WebsiteStatus/Worker.cs
@@ -1,20 +1,33 @@
 namespace WebsiteStatus
 {
+    /// <summary>
+    /// The worker is everything it needs to do
+    /// </summary>
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
 
+        /// <summary>
+        /// Logging info
+        /// </summary>
+        /// <param name="logger"></param>
         public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// This is what runs
+        /// </summary>
+        /// <param name="stoppingToken"></param>
+        /// <returns></returns>
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            // pass in a cancel token to stop the process
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now); // write a log entry
+                await Task.Delay(1000, stoppingToken); // wait a second
             }
         }
     }
